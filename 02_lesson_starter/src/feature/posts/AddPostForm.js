4c1151607a04a5445c7f4ddfb76cfe2058a7896f
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2024-06-20 04:54:48 +0530

    form updated to save data on backend

--- 02_lesson_starter/src/feature/posts/AddPostForm.js
+++ 02_lesson_starter/src/feature/posts/AddPostForm.js
@@ -1,37 +1,46 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
   const dispatch = useDispatch();
   const users = useSelector(selectAllUsers);
 
- 
-
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setuserId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
+
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
 
   const handleButtonClick = () => {
-    dispatch(postAdded({ title, content, userId }));
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
 
-    setTitle("");
-    setContent("");
-    setuserId("");
+        setTitle("");
+        setContent("");
+        setuserId("");
+      } catch (err) {
+        console.error("Failed to save the post", err);
+      } finally {
+        setAddRequestStatus("idle");
+      }
+    }
   };
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
   const onAuthorChanged = (e) => setuserId(e.target.value);
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
 
-   const usersOptions = users.map((user) => (
-     <option key={user.id} value={user.id}>
-       {user.name}
-     </option>
-   ));
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   return (
     <section>
