63493e125f910591459eb295eec614b393e44450
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2024-06-20 04:49:40 +0530

    fetching users from backend

--- 02_lesson_starter/src/feature/users/usersSlice.js
+++ 02_lesson_starter/src/feature/users/usersSlice.js
@@ -1,15 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  { id: "0", name: "Dude Lebowski" },
-  { id: "1", name: "Neil Young" },
-  { id: "2", name: "Dave Gray" },
-];
+const USER_URL = "http://localhost:3000/users";
+
+export const fetchUsers = createAsyncThunk("posts/fetchUsers", async () => {
+  const response = await axios.get(USER_URL);
+  if (response.status !== 200) {
+    throw new Error("Failed to fetch posts");
+  }
+  return response.data;
+});
+
+
+const initialState = [];
 
 const usersSlice = createSlice({
   name: "users",
   initialState: initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
